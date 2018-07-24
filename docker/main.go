3390b958aed22425669547340aa735a92925e45d
Roman Progonnyj <roman.progonnyj@gmail.com>
2018-07-24 21:46:46 +0300

    fix2

--- docker/main.go
+++ docker/main.go
@@ -1,54 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "html"
-    "log"
-    "net/http"
     "bufio"
     "os"
-
-    "github.com/sevlyar/go-daemon"
+    "fmt"
 )
 
 // To terminate the daemon use:
 //  kill `cat pid`
 
 func main() {
-    cntxt := &daemon.Context{
-	PidFileName: "pid",
-	PidFilePerm: 0644,
-	LogFileName: "log",
-	LogFilePerm: 0640,
-	WorkDir:     "./",
-	Umask:       027,
-	Args:        []string{"[go-daemon sample]"},
-    }
-
-    d, err := cntxt.Reborn()
-    if err != nil {
-	log.Fatal("Unable to run: ", err)
-    }
-    if d != nil {
-	return
-    }
-    defer cntxt.Release()
-
-    log.Print("- - - - - - - - - - - - - - -")
-    log.Print("daemon started")
-
-    serveHTTP()
     reader := bufio.NewReader(os.Stdin)
     text, _ := reader.ReadString('\n')
-    log.Print(text)
+    fmt.Printf(text)
 }
 
-func serveHTTP() {
-    http.HandleFunc("/", httpHandler)
-    http.ListenAndServe("127.0.0.1:8080", nil)
-}
-
-func httpHandler(w http.ResponseWriter, r *http.Request) {
-    log.Printf("request from %s: %s %q", r.RemoteAddr, r.Method, r.URL)
-    fmt.Fprintf(w, "go-daemon: %q", html.EscapeString(r.URL.Path))
-}
