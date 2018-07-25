1a288eb65822e9f581ee31fd2a3493aadb8dd1e1
Roman Progonnyj <roman.progonnyj@gmail.com>
2018-07-25 12:54:25 +0300

    pull req 4

--- docker/main.go
+++ docker/main.go
@@ -14,6 +14,6 @@ func main() {
     text, _ := reader.ReadString('\n')
     fmt.Printf(text)
     fmt.Printf("Helo Build")
-    fmt.Printf("Helo Pull request 3!")
+    fmt.Printf("Helo Pull request 4!")
 }
 
