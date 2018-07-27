70f0ab25c87ec8e51c7c28f3bc9cc475c681435b
Roman Progonnyj <roman.progonnyj@gmail.com>
2018-07-27 11:04:35 +0300

    new pipeline, added tests

--- docker/main.go
+++ docker/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"log"
+	"os"
 )
 
 // To terminate the daemon use:
 //  kill `cat pid`
 
 func main() {
-    fmt.Println("Helo Pull request 10")
-    fmt.Println("goodbye cruel world")
-//    BAG IS COMING!!
-}
+	fmt.Println("Helo Pull request 10")
+	if (len(os.Args) > 1) && (os.Args[1] == "tests") {
+                fmt.Println("Run tests...")
+		file, err := os.Create("result.txt")
+		if err != nil {
+			log.Fatal("Cannot create file", err)
+		}
+		defer file.Close()
 
+		fmt.Fprintf(file, "Results OK")
+		// os.Exit(1)
+	}
+	fmt.Println("goodbye cruel world")
+}
